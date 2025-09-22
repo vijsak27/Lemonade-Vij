eb72663bc42b81ba96c04f4747422b1d66cab6b4
Vij <vijsak27@7hills.org>
2025-09-22 08:45:56 -0400

    Day one (9/22) work

--- Main.java
+++ Main.java
@@ -1,8 +1,45 @@
+//imports
+import java.util.Scanner
+
+
 public class Main{
-	public statuc void main(String[] args) {
+	public static void main(String[] args) {
 		lemonadeStand l1 = new LemonadeStand();
 			l1.start();
 		
 	}
-	public  
+	public class Inventory (
+		private int money = 20;
+		private int lemons;
+		private int cups;
+		private int sugarCubes;
+		private int iceCubes;
+		System.out.println("You have "+ money + "dollars");
+		
+		System.out.println("How many lemons do you want to purchase: "); 
+		Scanner scan = new Scanner(system.in);
+		int buy_lemons = scan.nextInt();
+		String temp = scan.nextLine();
+		System.out.println("How many cups do you want to purchase: ");
+		int buy_cups = scan.nextInt();
+		String temp = scan.nextLine();
+		System.out.println("How many sugar cubes do you want to purchase: ");
+		int buy_sugar = scan.nextInt();
+		String temp = scan.nextLine();
+		System.out.println("How many ice cubes do you want to purchase: ");
+		int buy_ice = scan.nextInt();
+		String temp = scan.nextLine();
+		scan.close();
+		System.out.println("Your current inventory:")
+		
+		public void updateInventory(){
+			lemons += buy_lemons;
+			cups += buy_cups;
+			sugarCubes += buy_sugar;
+			iceCubes += buy_ice;
+			}
+		
+
+	)
+	
 }
