72244b04fcaa0bd554b01db87ec0aa07b10f6322
Vij <vijsak27@7hills.org>
2025-09-30 09:34:11 -0400

    updatd the main class9/30

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.lang;
 
 public class Main{
 	public static void main(String[] args) {
-		lemonadeStand l1 = new LemonadeStand();
+		LemonadeStand l1 = new LemonadeStand();
 			l1.start();	
 	}
 	
